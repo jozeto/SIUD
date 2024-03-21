921e6265b336cfa6977fa4c69baf27fae8077ae8
jozeto <sebastiansalgado404@gmail.com>
2024-03-21 14:26:51 -0500

    aaA

--- PuntoVenta/PuntoVenta/settings.py
+++ PuntoVenta/PuntoVenta/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
